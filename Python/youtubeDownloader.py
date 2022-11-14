5dba110baa0582be26002cbd46d3e0ae6c237791
Théo Laperrouse <theo.laperrouse@laposte.net>
2022-11-14 23:01:54 +0100

    Add some Fixes to historical code

--- Python/youtubeDownloader.py
+++ Python/youtubeDownloader.py
@@ -5,7 +5,7 @@ def download_videos(url: str, outpath: str = "./"):
     yt = YouTube(url)
     print('Le téléchargement de la vidéo a commencé...')
     yt.streams.filter(file_extension="mp4").get_by_resolution(
-        "720p").download(outpath)
+        "360p").download(outpath)
     print(
         f'Le téléchargement de la vidéo est fini, vous pouvez la retrouver dans le dossier {outpath}')
 
