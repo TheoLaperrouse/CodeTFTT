240ffbbadc06ee2433a830b32ac071e30a2246ca
Théo Laperrouse <theolaperrouse@gmail.com>
2023-02-27 20:37:17 +0100

    Update AppScript

--- AppsScript/getData.js
+++ AppsScript/getData.js
@@ -1,15 +1,16 @@
 function getMatchesWithJoueurs() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet = ss.getSheetByName('Rencontres');
-  compteur = 2
+  var compteur = 2
   const regex = /(THORIGNE-FOUILLARD TT)|(THORIGNE FOUILLARD TT)|^(THORIGNE TT)|^(TFTT)/;
   var response = UrlFetchApp.fetch('https://tftt.barais.fr/teams/allmatches/phase=2/joueurs=true', {'muteHttpExceptions': true});
   var matches = JSON.parse(response.getContentText());
   matches.map(function(match) {
     const numEquipe = match.equipeA.match(regex) ? match.equipeA.match(/\d+/g).join('') : match.equipeB.match(/\d+/g).join('')
     const numEquipeFinal =  match.equipeA.match(/^Féminines/) || match.equipeB.match(/^Féminines/)? parseInt(numEquipe) + 15 : numEquipe
+    const equipe = `${match.equipeA.replace(regex, 'TFTT')} vs ${match.equipeB.replace(regex, 'TFTT')}`
     sheet.getRange(compteur,1).setValue(match.date);
-    sheet.getRange(compteur,2).setValue(`${match.equipeA.replace(regex, 'TFTT')} vs ${match.equipeB.replace(regex, 'TFTT')}`);
+    sheet.getRange(compteur,2).setValue(equipe);
     sheet.getRange(compteur,3).setValue(numEquipeFinal);
     sheet.getRange(compteur,4).setValue(match.equipeA.match(regex) ? 'Domicile':'Extérieur');
     if(match.scoreA !== ''){
@@ -27,12 +28,25 @@ function getMatchesWithJoueurs() {
     }
     compteur += 1;
   })
+  supprProA()
+}
+
+function supprProA() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getSheetByName('Rencontres');
+  var data = sheet.getDataRange().getValues();
+  var numRows = sheet.getLastRow();
+  for (var i = numRows; i > 0; i--) {
+    if (data[i-1][11] === "Pro A") {
+      sheet.deleteRow(i);
+    }
+  }
 }
 
 function getProAStats(){
   var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.getSheetByName('TestTheo');
-  var response = UrlFetchApp.fetch('http://54.154.202.254/proA', {'muteHttpExceptions': true});
+  var sheet = ss.getSheetByName('Performance Pro A');
+  var response = UrlFetchApp.fetch('http://fastapifftt.thorigne-tt.net/proA', {'muteHttpExceptions': true});
   var joueurs = JSON.parse(response.getContentText());
   compteur = 2
   joueurs.map(function(joueur) {
